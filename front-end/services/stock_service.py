b173378cf07c8cabbdb9df4a7fd3c4693f09410d
Kafka Wanna Fly <tarzanchemgio@gmail.com>
2022-08-27 16:16:22 +0700

    Fix exported function

--- front-end/services/stock_service.py
+++ front-end/services/stock_service.py
@@ -18,14 +18,14 @@ def schedule_download_stock(stockSymbol: str, interval: str, df: pd.DataFrame):
     scheduler = BackgroundScheduler()
 
     scheduler.add_job(
-        lambda: download_stock(stockSymbol, interval, df), 'interval', seconds=seconds)
+        lambda: _download_stock(stockSymbol, interval, df), 'interval', seconds=seconds)
 
     scheduler.start()
 
     return scheduler
 
 
-def download_stock(stockSymbol: str, interval: str, df: pd.DataFrame):
+def _download_stock(stockSymbol: str, interval: str, df: pd.DataFrame):
     print(f"Downloading {stockSymbol} - {interval}")
     df = yf.download(stockSymbol, period=get_period_from_interval(
         interval), interval=interval)
